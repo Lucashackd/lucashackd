b3d8db3bf5c0d52d35af4ef5f6368d1084034bcc
Lucas Hackbart Döhnert <lucashackd@gmail.com>
2025-11-09 00:08:35 -0300

    style: update Footer component for improved text styling and layout

--- src/app/_components/_Footer/footer.tsx
+++ src/app/_components/_Footer/footer.tsx
@@ -6,8 +6,8 @@ const kodeMono = Kode_Mono({ subsets: ["latin"] });
 function Footer() {
   return (
     <div className="relative z-10 flex w-full items-center justify-center bg-violet-900 p-6 shadow-[0_-4px_10px_rgba(0,0,0,0.6)]">
-      <p className="flex gap-2 text-lg text-amber-200">
-        Made with <Heart className="text-rose-300" />{" "}
+      <p className="text-sm sm:text-lg xl:text-2xl text-amber-200 text-center">
+        Made with <Heart fill="#2E1065" className="inline-block text-rose-300 size-[0.875rem] sm:size-[1.125rem]" />{" "}
         <span className={`${kodeMono.className} text-sky-300`}>
           (and Next.js)
         </span>{" "}
